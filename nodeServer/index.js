ca3aa4aff73edaae3df0c344b52275b3db207842
Chhavi Sinha <chhavi823lmw@gmail.com>
2023-08-01 02:01:35 +0530

    Initial Commit

---
+++ nodeServer/index.js
@@ -0,0 +1,19 @@
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.braodcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.braodcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+
+    socket.on('disconnect', message => {
+        socket.braodcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+});
